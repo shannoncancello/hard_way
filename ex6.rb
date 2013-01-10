b8990c637631856cdc1f467b44e5fac113ae5045
Shannon Cancello <shannon.cancello@gmail.com>
2013-01-09 22:36:19 -0500

    Ex 6 with a little extra

---
+++ ex6.rb
@@ -0,0 +1,22 @@
+x = "There are #{10} types of people."
+binary = "binary"
+do_not = "don't"
+#two strings are here
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+#a string is here
+puts "I said #{x}."
+#a string is here
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
