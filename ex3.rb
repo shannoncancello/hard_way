ae3f50e807f8f12981d0761365a7874d8d072464
Shannon Cancello <shannon.cancello@gmail.com>
2013-01-09 21:22:44 -0500

    Ex 3 with some extra

---
+++ ex3.rb
@@ -0,0 +1,34 @@
+puts "I will now count my chickens:"
+
+#30 is first divided by 6 and then that number (5) is added to 25
+puts "Hens", 25 + 30 / 6
+#The first part of the equation is the multiplication of 25 by 3
+#Then the remainder of the division (3) is subtracted from 100
+puts "Roosters", 100 - 25 * 3 % 4
+
+
+puts "Now I will count the eggs:"
+#First parts of this eqaution is the divison of 4 by 2
+#That remainder is 0, and then 1 is divided by 4
+#And because it is a flaoting point number it gets rounded to zero
+#You are now left with 3 + 2 + 1 (6)
+#6 - 5 is 1, added to 6 is 7
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+#This one is pretty much mapped out
+puts 3 + 2 < 5 - 7
+
+
+puts "What is 3 + 2?", 3 + 2
+puts "What is 5 - 7?", 5 - 7
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+#Just feeding in the system equations that will produce true or false results
+
+puts "Is it greater?", 5 > -2
+puts "Is it greator or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
