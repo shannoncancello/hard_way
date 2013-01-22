74c347dbe6636a0bc23807ab301750e5796c5edf
shannoncancello <shannon.cancello@gmail.com>
2013-01-21 21:30:33 -0500

    ex 34 as is

---
+++ ex34.rb
@@ -0,0 +1,9 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+#the animal at 1 is the 2nd animal and is a python
+#the 3rd animal is at 2 and is a peacock
+#the first animal is at 0 and is a bear
+#the animal at 3 is the fourth animal and is a kangaroo
+#the fifth animal is at 4 and is a whale
+#the animal at two is the third animal and is a peacock
+#the sixth animal is at 5 and is a platypus
+#the animal at 4 is the fifth animal and is a whale
